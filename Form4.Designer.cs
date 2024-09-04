bfb5426e5a9ecefb0f125a4cda5640fbcd456ce9
OnlyPray31 <146093632+OnlyPray31@users.noreply.github.com>
2024-09-04 12:04:07 +0800

    Add files via upload

---
+++ Form4.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace form_login
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.quit = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // quit
+            // 
+            this.quit.BackColor = System.Drawing.Color.HotPink;
+            this.quit.FlatAppearance.BorderSize = 0;
+            this.quit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.quit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quit.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.quit.Location = new System.Drawing.Point(272, 401);
+            this.quit.Margin = new System.Windows.Forms.Padding(2);
+            this.quit.Name = "quit";
+            this.quit.Size = new System.Drawing.Size(99, 38);
+            this.quit.TabIndex = 68;
+            this.quit.Text = "Exit";
+            this.quit.UseVisualStyleBackColor = false;
+            this.quit.Click += new System.EventHandler(this.quit_Click);
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(12, 62);
+            this.Output.Multiline = true;
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(357, 334);
+            this.Output.TabIndex = 59;
+            this.Output.TextChanged += new System.EventHandler(this.Output_TextChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Pink;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(381, 42);
+            this.panel2.TabIndex = 49;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.HotPink;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.label2.Location = new System.Drawing.Point(107, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "BIODATA OUTPUT";
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 450);
+            this.Controls.Add(this.quit);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button quit;
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Form4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form_login
+{
+    public partial class Form4 : Form
+    {
+        public Form4(String outputText)
+        {
+            InitializeComponent();
+            Output.Text = outputText;
+        }
+
+        private void quit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Output_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
